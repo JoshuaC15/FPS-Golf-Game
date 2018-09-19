2159a6c43271784041a969d2ac98d2d62ef23e24
JoshuaC15 <34174228+JoshuaC15@users.noreply.github.com>
2018-09-18 23:34:28 -0400

    Add files via upload

---
+++ CannonShoot.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonShoot : MonoBehaviour {
+
+	public GameObject cannonBall;
+
+	public float firePower;
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void ShootCannon () {
+		GameObject thisCannonBall = Instantiate (cannonBall, transform.position, transform.rotation);
+		thisCannonBall.GetComponent <Rigidbody> ().AddRelativeForce (0, 0, firePower, ForceMode.Impulse);
+	}
+
+}
---
+++ MyScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class MyScript : MonoBehaviour {
+
+
+	public FixedButton JumpButton;
+	public FixedTouchField TouchField;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		var fps = GetComponent<RigidbodyFirstPersonController>();
+
+
+		fps.JumpAxis = JumpButton.Pressed;
+		fps.mouseLook.LookAxis = TouchField.TouchDist;
+		
+	}
+}
